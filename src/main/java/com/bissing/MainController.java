311ef7c50426ef8da37d847c732a05f22c55746f
AlexBissing <avbissing@gmail.com>
2022-06-02 18:50:27 +0300

    initial commit

---
+++ src/main/java/com/bissing/MainController.java
@@ -0,0 +1,23 @@
+package com.bissing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/view/{name}")
+    public String view(@PathVariable("name") String name,  Model model) {
+        model.addAttribute("msg", "Hello" + name);
+        return "index";
+    }
+
+    @GetMapping("/raw")
+    @ResponseBody
+    public String raw() {
+        return "Raw data";
+    }
+}
